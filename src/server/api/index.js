5ee747fbd0436677e07cc5c11b529782501e376d
Volodymyr Cholak <volchok60@yahoo.com>
2018-03-03 14:39:25 -0500

    Webpack config uses web/node targets.

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,4 +1,4 @@
-import { version } from '../../package.json';
+import { version } from '../../../package.json';
 import { Router } from 'express';
 import products from './products';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,52 @@
-const path = require('path');
+var path = require('path');
 
-module.exports = {
-    entry: './src/ui/app.jsx',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                test: /\.jsx$/,
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+var serverConfig = {
+  target: 'node',
+  entry: './src/server/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',                
+        exclude: /node_modules/
+      }      
+    ]
+  }
 };
+
+var clientConfig = {
+  target: 'web', // <=== can be omitted as default is 'web'
+  entry: './src/ui/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',                
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true
+  }
+};
+
+module.exports = [ serverConfig, clientConfig ];
